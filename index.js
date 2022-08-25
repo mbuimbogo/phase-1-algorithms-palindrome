be5e5a9f42c92a47d20c5e2fa1aed33be637bebb
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-08-25 16:44:42 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // write the problem in my own word
+  // write testcases
+  // have my pseudocode
+  // convert pseudocode map to code!
+  // make a clean and readable code
+  let newArray = word.split('');
+  let reversedNewArray = newArray.reverse();
+  let reversedString = reversedNewArray.join('')
+
+  if (word === reversedString){
+    return true;
+  }
+  else {
+    return false
+  }
+
 }
+console.log(isPalindrome('bob'))
 
 /* 
   Add your pseudocode here
+    let newArray = word.split('');
+  let reversedNewArray = newArray.reverse();
+  let reversedString = reversedNewArray.join('')
+
+  if (word === reversedString){
+    return true;
+  }
+  else {
+    return false
+  }
+
+}
+console.log(isPalindrome('bob'))
 */
 
 /*
   Add written explanation of your solution here
+  have a function isPalindrome with a string as the argument
+  split the string into array
+  reverse the array
+  join the split array to a new string
+  initialize if else
 */
 
 // You can run `node index.js` to view these console logs
